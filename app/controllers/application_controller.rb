bdd293ec0037dd2bf2e062bb66b33d6e216a622e
sakuuuuuuuu <themesong99@gmail.com>
2023-10-13 18:08:07 +0900

    新規登録/ログインのパスの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
@@ -11,8 +11,10 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    end
   end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name_last, :name_first, :name_last_kana,:name_first_kana,:birth_day])
+    end
+  
 end
