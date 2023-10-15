b408fcd5349b0b3979d49a2d1564e84ed9d1cfa3
sakuuuuuuuu <themesong99@gmail.com>
2023-10-15 20:30:49 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :name_last, presence: true
-         validates :name_first, presence: true
-         validates :name_last_kana, presence: true
-         validates :name_first_kana, presence: true
+         validates :name_last, presence: true ,format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :name_first, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :name_last_kana, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :name_first_kana, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
          validates :birth_day, presence: true
+         validates :password, format:{ with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
+
+
+
           # email と  password はデバイスで生成されてるからバリデーション記述する必要なし
   end
 
