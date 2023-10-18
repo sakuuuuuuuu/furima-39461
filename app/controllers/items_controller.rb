86227eff697dc3c6fc7517693acc1b763e54ee1e
sakuuuuuuuu <themesong99@gmail.com>
2023-10-18 20:19:21 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,9 @@ class ItemsController < ApplicationController
   def index  # indexアクションを定義した
   end
 
+  def new
+  end
+
   # def move_to_index
   #   unless user_signed_in?
   #     redirect_to action: :index
