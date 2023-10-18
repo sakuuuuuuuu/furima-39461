e92f860452909c0791016d582814fe0ec6ab1d80
sakuuuuuuuu <themesong99@gmail.com>
2023-10-18 20:19:14 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,19 @@ class Item < ApplicationRecord
   belongs_to :prefecture_id
   belongs_to :days_until_shipping_id
 
-  
+
   has_one :purchase_record
   belongs_to :user
+
+
+  validates :name, presence: true
+  validates :explaination, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :del_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_until_shipping_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
+  
 end
