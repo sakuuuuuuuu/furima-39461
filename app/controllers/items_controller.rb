57b2ec6c1b11d5fb0d75711629ad453f80535d64
sakuuuuuuuu <themesong99@gmail.com>
2023-10-26 16:43:57 +0900

    beforeアクションの纏め忘れ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   
 
@@ -34,7 +34,6 @@ def edit
 end
 
 def update
-  @item = Item.find(params[:id])
   if @item.update(item_params)
     redirect_to item_path
   else
